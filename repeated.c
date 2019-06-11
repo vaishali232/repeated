3c5894d0d84db69ca66baf2d306f32d23759a130
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-06-11 14:02:01 +0530

    Update repeated.c

--- repeated.c
+++ repeated.c
@@ -2,7 +2,7 @@
 int main()
 {
 
-int n,t,k=1;
+int n,t,k=1,flag=0;
 scanf("%d",&n);
 int a[n];
 int freq[n];
@@ -22,9 +22,14 @@ for(int i=0;i<n;i++)
 {
 if(freq[i]>1)
 {
+	flag=1;
 printf("%d ",i);
 }
 
 }
+	if(flag==0)
+	{
+		printf("unique");
+	}
 
 }

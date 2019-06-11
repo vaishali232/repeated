28f7cf5513aeaddddb5a2ae068cd9375d5d6264f
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-06-11 13:58:08 +0530

    Create repeated.c

---
+++ repeated.c
@@ -0,0 +1,30 @@
+##include<stdio.h>
+int main()
+{
+
+int n,t,k=1;
+scanf("%d",&n);
+int a[n];
+int freq[n];
+for(int i=0;i<n;i++)
+{
+scanf("%d ",&a[i]);
+}
+for(int i=0;i<n;i++)
+{
+	freq[i]=0;
+}
+for(int i=0;i<n;i++)
+{
+freq[a[i]]++;
+}
+for(int i=0;i<n;i++)
+{
+if(freq[i]>1)
+{
+printf("%d ",i);
+}
+
+}
+
+}
